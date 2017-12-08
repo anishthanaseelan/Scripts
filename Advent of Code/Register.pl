cb5a618bb20851dfe6a86a1e5fccb360eff5a2ce
Anish Thanaseelan Kaspaar <anishthanaseelan@gmail.com>
2017-12-08 23:10:00 +0000

    Two start script

--- Advent of Code/Register.pl
+++ Advent of Code/Register.pl
@@ -1,34 +1,83 @@
 my $File = CopyFile("registertest.txt");
-my  $exec = 0;
+my $Register = PerformInst($File);
+
+PrintRegister ( $Register );
+PrintBiggest ( $Register );
+
+sub PrintBiggest(){
+  $Register = shift;
+  my $bigval = 0;
+  my $bigvar = undef;
+  foreach my $variable ( keys %{$Register}){
+    if ( $bigval < $Register->{$variable} ){
+        $bigval = $Register->{$variable};
+        $bigvar = $variable;
+    }
+  }
+  print "<BIG VAR> <$bigvar> < BIG VAL> <$bigval> \n"
+}
+
+
+sub PrintRegister(){
+  $Register = shift;
+  foreach my $variable ( sort keys %{$Register}){
+    print "$variable->". $Register->{$variable} . "\n";
+  }
+}
+
+sub PerformInst(){
+
+my $File = shift;
+my %Variable = () ;
+my $exec = 0;
+my $bigval = 0;
+my $bigvar = undef;
 
 foreach $ins (@{$File}){
    chomp($ins);
    $exec = 0;
    if ( $ins =~ m/^(\w+) (\w+) (-?\d+) if (\w+) (.*) (-?\d+)$/ ){
-      print " <$1> <$2> <$3><$4><$5><$6> \n";
-      $Variable{$1} = 0 if ( ! exists $Variable{$1}  );
-         switch ( $5 ){
-=Anish
-           case "\>" { $exec = 1 if ( $4 > $6 ) ; }
-           case "\<" { $exec = 1 if ( $4 < $6 ) ; }
-           case "\>\=" {$exec = 1 if ( $4 >= $6 ) ;}
-           case "\<\=" {$exec = 1 if ( $4 <=  $6 ) ; }
-           case "\=\=" { $exec = 1 if ( $4 == $6 ) ;}
-           case "\!\=" { $exec = 1 if ( $4 != $6 ) ; }
-           else { die  (" UNHANDLED CONDITION   <$1>\n" ); }
-=cut
-         }
-         if ( $exec ) {
-             if ( $2 == "inc" ){
-             $Variable{$1} = $Variable{$1} + $3;
-             } else {
-              $Variable{$1} = $Variable{$1} - $3;
-             }
-         }
+      $var = $1;
+      $op = $2;
+      $offset = $3;
+      $refvar = $4;
+      $refcon = $5;
+      $refval = $6;
+      print " <$var> <$op> <$offset><$refvar><$refcon><$refval> \n";
+      $Variable{$var} = 0 if ( !exists $Variable{$var}  );
+      $Variable{$refvar} = 0 if ( !exists $Variable{$refvar}  );
+
+      if ( $refcon eq ">"){ $exec = 1 if ( $Variable{$refvar} > $refval ) ; }
+      elsif ( $refcon eq "<"){ $exec = 1 if ( $Variable{$refvar} < $refval ) ; }
+      elsif ( $refcon eq ">="){ $exec = 1 if ( $Variable{$refvar} >= $refval ) ;}
+      elsif ( $refcon eq "<="){ $exec = 1 if ( $Variable{$refvar} <= $refval ) ;}
+      elsif ( $refcon eq "=="){ $exec = 1 if ( $Variable{$refvar} == $refval ) ;}
+      elsif ( $refcon eq "!="){ $exec = 1 if ( $Variable{$refvar} != $refval ) ;}
+      else { die  (" UNHANDLED CONDITION   <$refcon>\n" ); }
+
+      if ( $exec ) {
+
+          if ( $op eq "inc" ){
+             $Variable{$var} = $Variable{$var} + $offset;
+          } elsif ( $op eq "dec" ){
+              $Variable{$var} = $Variable{$var} - $offset;
+          } else { die  (" UNHANDLED OPERATION <$op>\n" ); }
           $exec = 0;
-   } else {
-    die  (" UNHANDLED INSTRUCTION <$ins>\n" );
-   }
+      # To find the highest Value 
+         if ( $bigval < $Variable{$var} ){
+         $bigval = $Variable{$var};
+         $bigvar = $var;
+    }
+
+      }
+      
+    } else {
+      die  (" UNHANDLED INSTRUCTION <$ins>\n" );
+    }
+}
+  print "<WHILE EXEC><BIG VAR> <$bigvar> < BIG VAL> <$bigval> \n";
+
+return \%Variable;
 }
 
 sub CopyFile()
